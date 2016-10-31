1a6c244d604757955495a1d9b4df53c918129e72
rdalvarez <david_de_odin@hotmail.com>
2016-10-31 19:53:46 -0300

    agregue stilos

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <html>
 <head>
-	<title></title>
+	<title>clase08</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	
 	<script type="text/javascript" src="js/funciones.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
 <body>
-	<div id="frmIngreso">
+	<div id="frmIngreso" class="widget clearfix">
 		<input type="text" id="user" placeholder="User">
+		<br>
 		<input tpye="text" id="pass" placeholder="Pass">
-		<input type="button" onclick="Ingresar()" value="Ingresar">
+		<br>
+		<input type="button" onclick="Ingresar()" value="Ingresar" class="btn">
 	</div>
 	<div id="test"></div>
 </body>
